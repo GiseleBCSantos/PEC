79cfabf4e9b5db48b5a4a7a3be266689b04fbd5e
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-03-14 09:25:30 -0300

    Update Sem5At1Q2.py

--- Sem5At1Q2.py
+++ Sem5At1Q2.py
@@ -5,10 +5,10 @@ def perimetro(x):
     return x*4
 
 def main():
-    lado = float(input())
+    lado = float(input("Insira um valor para o lado do quadrado: "))
 
-    print(f"{area(lado):10.4f}")
-    print(f"{perimetro(lado):10.4f}")
+    print(f"A área do quadrado é de: {area(lado):10.4f}")
+    print(f"O perimetro do quadrado é de: {perimetro(lado):10.4f}")
 
 __name__ = '__main__'
 main()
