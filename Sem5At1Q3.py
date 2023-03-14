b792c1d7d6eed1a18c501c8bb499454bd22b823b
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-03-14 09:27:33 -0300

    Update Sem5At1Q3.py

--- Sem5At1Q3.py
+++ Sem5At1Q3.py
@@ -5,11 +5,11 @@ def desconto(x,y):
     return x* (1-(y/100))
 
 def main():
-    valor = float(input())
-    v_d = float(input())
+    valor = float(input("Insira um valor para ser feito o cálculo de aumento e desconto: "))
+    v_d = float(input("Insira uma porcentagem para o cálculo: "))
 
-    print(f"{aumento(valor,v_d):.2f}")
-    print(f"{desconto(valor, v_d):.2f}")
+    print(f"O aumento do valor de {valor} com {v_d}% foi de: {aumento(valor,v_d):.2f}")
+    print(f"O desconto do valor de {valor} com {v_d}% de desconto foi de: {desconto(valor, v_d):.2f}")
 
 __name__ = '__main__'
 main()
