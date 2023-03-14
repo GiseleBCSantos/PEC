4368efa6bb22fc0e9ba3ce07a7d9898ce4de36fc
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-03-14 09:24:23 -0300

    Update Sem5At1Q1.py

--- Sem5At1Q1.py
+++ Sem5At1Q1.py
@@ -2,11 +2,11 @@ def calcular(x,y,z):
     return 2*x + 5*y - z
 
 def main():
-    a = int(input())
-    b = int(input())
-    c = int(input())
+    a = int(input("Insira um valor 'a' para o calculo: "))
+    b = int(input("Insira um valor 'b' para o calculo: "))
+    c = int(input("Insira um valor 'c' para o calculo: "))
 
-    print(calcular(a,b,c))
+    print(f"O resultado da equação é de {calcular(a,b,c)}")
 
 __name__ = '__main__'
 main()
