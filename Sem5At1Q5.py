9bee29204a1a91b9d97d20a9ade86b9fb9af5d39
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-03-15 08:04:37 -0300

    Update Sem5At1Q5.py

--- Sem5At1Q5.py
+++ Sem5At1Q5.py
@@ -1,10 +1,32 @@
+# Como eu fiz:
 def inverter(x):
     return x[::-1]
 
 def main():
-    num = input("Insira um número para ser invertido: ")
+    num = input("Insira um número entre 1000 e 9999 para ser invertido: ")
 
     print(f"O numero {num} invertido fica: {inverter(num)}")
+
+__name__ = '__main__'
+main()
+
+#Como imagino que o professor quer:
+def inverter(x):
+    u = x % 10
+    x = x // 10
+    d = x % 10
+    x = x // 10
+    c = x % 10
+    x = x//10
+    m = x
+    n_i = (u*1000) + (d*100) + (c*10) + m
+    return n_i
+
+def main():
+    num = int(input("Insira um número entre 1000 e 9999 para ser invertido: "))
+
+    n_i = inverter(num)
+    print(f"O inverso de {num} é {n_i}")
 
 __name__ = '__main__'
 main()
