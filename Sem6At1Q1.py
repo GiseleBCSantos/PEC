025a197355223fa3904bbea4cb510d092b39eda0
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-03-20 16:34:31 -0300

    Update Sem6At1Q1.py

--- Sem6At1Q1.py
+++ Sem6At1Q1.py
@@ -2,9 +2,9 @@ def quantidade_de_caracteres(x):
     return len(x)
 
 def main():
-    nome = input().strip()
+    nome = input("Insira seu nome pra saber quantos caracteres ele tem: ").strip()
 
-    print(quantidade_de_caracteres(nome))
+    print(f"Seu nome tem {quantidade_de_caracteres(nome)} caracteres.")
 
 if __name__ == '__main__':
     main()
