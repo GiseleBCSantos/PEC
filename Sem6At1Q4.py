8ebb26d5e5033606b9a54736bf430531ec71869b
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-03-20 16:03:21 -0300

    Update Sem6At1Q4.py

--- Sem6At1Q4.py
+++ Sem6At1Q4.py
@@ -1,23 +1,23 @@
 def main():
     numeros = []
-    n1 = int(input())
+    n1 = int(input("Insira um número: "))
     numeros.append(n1)
-    n2 = int(input())
+    n2 = int(input("Insira um número: "))
     numeros.append(n2)
-    n3 = int(input())
+    n3 = int(input("Insira um número: "))
     numeros.append(n3)
-    n4 = int(input())
+    n4 = int(input("Insira um número: "))
     numeros.append(n4)
-    n5 = int(input())
+    n5 = int(input("Insira um número: "))
     numeros.append(n5)
 
     maximo = max(numeros)
     minimo = min(numeros)
     media = sum(numeros)/5
 
-    print(maximo)
-    print(minimo)
-    print(media)
+    print(f"O maior número digitado foi: {maximo}.")
+    print(f"O menor número digitado foi: {minimo}.")
+    print(f"A média dos 5 números digitados é de: {media}.")
 
 
 if __name__ == "__main__":
