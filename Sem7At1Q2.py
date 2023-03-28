0f6a9a42c6afd79856079bdf7185099ae0866f75
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-03-28 10:45:15 -0300

    Update Sem7At1Q2.py

--- Sem7At1Q2.py
+++ Sem7At1Q2.py
@@ -1,11 +1,11 @@
-def eh_par(num):
+def eh_impar(num):
     return num % 2 == 1
 
 
 def main():
-    numero = int(input())
+    numero = int(input("Insira um número para saber se ele é ímpar ou não (True para ímpar e False para par): "))
 
-    print(eh_par(numero))
+    print(eh_impar(numero))
 
 
 if __name__ == '__main__':
