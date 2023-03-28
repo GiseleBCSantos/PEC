3675014e386279be5cbf6d8ac59c0e30c0bb9e64
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-03-28 10:51:49 -0300

    Update Sem7At1Q5.py

--- Sem7At1Q5.py
+++ Sem7At1Q5.py
@@ -1,16 +1,16 @@
 def calculo(x):
     if x[2] >= 8 and ((sum(x)/3) + 1) <=10:
-      return (sum(x)/3) + 1
+      return f"O aluno ganhou 1 ponto na média, e sua média ficou {(sum(x)/3) + 1}"
     elif (sum(x)/3) + 1 > 10:
-        return 10
+        return f"O aluno ganhou 1 ponto na média, e sua média ficou {10}"
     else:
-        return sum(x)/3
+        return f" A média do aluno é de {sum(x)/3}"
 
 
 def main():
     notas = []
     for i in range(3):
-        n = float(input())
+        n = float(input("Insira uma nota: "))
         notas.append(n)
 
     print(calculo(notas))
