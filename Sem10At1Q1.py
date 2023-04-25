d9dd292411cc4731cb8fb1fba88b68cc4e1ed214
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-04-25 17:34:00 -0300

    Create Sem10At1Q1.py

---
+++ Sem10At1Q1.py
@@ -0,0 +1,12 @@
+def num1a50():
+    for i in range(1,51):
+        print(i)
+
+
+def main():
+  print("Abaixo será impresso todos os numeros entre 1 e 50:")
+  num1a50()
+    
+    
+if __name__ == '__main__':
+    main()
